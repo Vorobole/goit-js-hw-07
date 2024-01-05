a1e8be6273c6ef745be461d447fd8b2ce3f87cbb
Vorobole <vorobyov1903@gmail.com>
2024-01-05 14:17:47 +0200

    task05 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorSpan = document.querySelector('.color');
+const changeColorButton = document.querySelector('.change-color');
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  colorSpan.textContent = randomColor;
+});
